4deef953eb697f0d31a41a932b1e852a89b445bd
Neha Thorat <thoratneha18@gmail.com>
2026-03-25 23:08:07 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,9 @@
+info={}  #empty dict
+days=set()  #empty set
+days={"saturday","sunday","sunday","sunday","saturday"}
+print(type(info))
+print(days)  #it will remove duplicates and gives us op
+
+nums = [1,1,1,1,2,2,2,3,3,4,6.4,6.4,0,-1,-4]
+nums = list(set(nums))
+print(nums)
