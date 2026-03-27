88c1888e12316773882d55b7f072ce367cc1c281
Neha Thorat <thoratneha18@gmail.com>
2026-03-27 23:29:32 +0530

    Create table.py

---
+++ table.py
@@ -0,0 +1,27 @@
+# num=int (input("Enter the num you want table for"))
+
+# #string formatting f
+# name=input("Dost ka name")
+# print (f" Dost ka name is {name}")
+# for i in range (1,11):
+#     print(f" {num} X {i} = {num*i}")
+
+# suraj="chand"
+# while suraj=="chand":
+#     print("You are best")
+#     break
+
+
+choice=("Enter the choice (presss q to quit) : ")
+
+while choice!="q":
+    num=int (input("Enter the num you want table for : "))
+    #for(i = 1; i < 11; i++)
+    for i in range (1,11):
+        print(f" {num} X {i} = {num*i}") 
+    choice=input("Enter the choice (presss q to quit) : ")                   
+
+
+
+
+    
