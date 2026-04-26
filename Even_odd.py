26177a815ba952f1aba64abd38d37f7a1250d6bf
Neha Thorat <thoratneha18@gmail.com>
2026-04-26 22:07:04 +0530

    Add Even/Odd checker in Even_odd.py

---
+++ Even_odd.py
@@ -0,0 +1,16 @@
+
+def CheckEven(No):
+    if(No % 2 == 0):
+        print("It is Even number")
+    else:
+        print("It is Odd number")
+
+def main():
+    Value = 0
+
+    print("Enter the number : ")
+    Value = int(input())
+
+    CheckEven(Value)
+
+main() 
